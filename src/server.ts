d9ee7076af24c3d4e239a12e1ab468c816043eb5
Rebecca Manzi <rebeccamanzi@gmail.com>
2022-07-01 10:00:30 -0300

    configure project with ts, node, eslint and prettier

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json()); // receber valores em json
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
+
+app.listen(3333, () => console.log('Server is running!'));
